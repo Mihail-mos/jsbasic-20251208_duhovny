6c4dabde4376e179186bbfbc29c57c5baadfcbfc
Mihail-mos <d-misha_1990@mail.ru>
2025-12-11 22:09:30 +0300

    task 1

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,10 @@
-function sum(m, n) {
-  // ваш код...
-}
+"use strict";
+function sum(a, b) {
+  return a + b;
+};
+
+let result1 = sum(1, 1);
+alert( result );
+
+let result2 = sum(2, 5);
+alert( result );
